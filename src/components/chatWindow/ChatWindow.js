98b287091ad48d7cf716ed3c336a0112191ea679
conmart <connor.a.martinelli@gmail.com>
2018-10-25 22:42:40 -0700

    working on transitions

--- src/components/chatWindow/ChatWindow.js
+++ src/components/chatWindow/ChatWindow.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import Message from '../message/Message';
 import styles from './ChatWindow.css';
+import { CSSTransition } from 'react-transition-group';
 
-const ChatWindow = ({messages}) =>
+const ChatWindow = ({messages}) => {
+  let convo = messages.map((message, index) =>
+    <Message
+      message={ message }
+      key={ index } />)
+
+  let createMessage = messages.length
+
+  return (
     <div className={styles.chatWindow}>
-      {messages.map((message, index) => {
-        return (
-          <Message
-            message={ message }
-            key={ index } />
-        )
-      })}
+      <CSSTransition
+        in={createMessage == true}
+        timeout={3000}
+        classNames='message'
+        unmountOnExit
+      >
+        <Message
+          message={messages[0]}
+        />
+      </CSSTransition>
     </div>
+  )
+}
 
 export default ChatWindow;
