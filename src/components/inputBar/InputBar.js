5a525d3dcf04b36ad75a54de4b2d02cb549f1c8e
conmart <connor.a.martinelli@gmail.com>
2018-10-31 22:27:31 -0700

    better styling

--- src/components/inputBar/InputBar.js
+++ src/components/inputBar/InputBar.js
@@ -7,7 +7,7 @@ const InputBar = ({newMessageSent, inputUpdated, value}) => {
       <form className={styles.form} onSubmit={newMessageSent}>
         <input className={styles.input} placeholder='Start typing...' name='check' onChange={inputUpdated} value={value}></input>
         <button className={styles.submitBtn} type='submit'>
-          <img className={styles.sendIcon} src="../../../static/images/paper-plane-white.png" width="40" />
+          <img className={styles.sendIcon} src="../../../static/images/paper-plane-white.png" />
         </button>
       </form>
     </div>
